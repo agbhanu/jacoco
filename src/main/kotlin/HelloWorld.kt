62daed62be2c7768a6e684c950e8415cf7501375
bhanu_agrawal <bhanu_agrawal@persistent.com>
2019-02-18 12:14:11 +0530

    Initial commit

---
+++ src/main/kotlin/HelloWorld.kt
@@ -0,0 +1,9 @@
+class HelloWorld {
+
+    public fun main(args:Array<String>){
+        println(HelloWorld().sumOfTwoNum(2,4))
+    }
+    public fun sumOfTwoNum(a:Int,b:Int):Int{
+        return a+b
+    }
+}
